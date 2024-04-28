05dd4dd4dd00145accf718e025c16e8ccd3ecb14
MohamedFazil89 <you@example.com>
2024-04-29 00:17:22 +0530

    register message

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { Sequelize, DataTypes } = require("sequelize");
 const bodyparser = require('body-parser');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -33,41 +34,114 @@ sequelize
   });
 
 //   model schema
-const post = sequelize.define("post", {
-  title: {
+const player = sequelize.define("player", {
+  username: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  content: {
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+const host = sequelize.define("host", {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
     type: DataTypes.STRING,
     allowNull: false,
   },
 });
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { role: roles, state: usestate });
 });
+
+let roles;
+var usestate;
 
 app.post("/submit", async (req, res) => {
-  const { title, content } = req.body;
+  const { username, password, email, role } = req.body;
   try {
-    const newPost = await post.create({ title, content });
-    res.json(newPost);
+    if (role === "player") {
+      const newPost = await player.create({ username, password, email });
+      // res.send("Succussfully Registered! as a Player");
+      roles = role;
+      usestate = true;
+      setTimeout(()=>{
+      res.redirect("/");
+      }, 5000)
+    } else {
+      const newPost = await host.create({ username, password, email });
+      // res.send("Succussfully Registered! as a Host");
+      roles = role
+      usestate = true;
+      setTimeout(()=>{
+        res.redirect("/");
+        }, 5000)
+
+
+    }
+
+    // res.json(newPost);
   } catch (err) {
     console.log(err);
+    usestate = false;
   }
 });
 
-app.get("/login", async (req, res) => {
+// app.get("/list-data", async (req, res) => {
+//   const { username, password, email } = req.body;
+//   try {
+//     const allPosts = await host.findAll();
+//     res.json(allPosts);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+app.post("/login", async (req, res) => {
+
+  const { username, password, role } = req.body;
+
   try {
-    const allPosts = await post.findAll();
-    res.json(allPosts);
+    if (role === "player") {
+      const user = await player.findOne({ where: { username, password } });
+      if ((user) && (password == user.password)) {
+        res.render("home.ejs");
+        console.log("login succussfull");
+
+      }
+    } else {
+      const user = await host.findOne({ where: { username, password } });
+      if ((user) && (password == user.password)) {
+        res.render("home.ejs");
+        console.log("login succussfull");
+      }
+
+    }
+
+  
+
   } catch (err) {
     console.log(err);
+    res.status(500).json({ error: "An error occurred during login" });
   }
-});
+})
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
